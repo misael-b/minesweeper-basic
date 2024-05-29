8f8057bf63b15b8d03d86717e34a2816e5a0f060
mbarcenas <misael.barcenas@yahoo.com>
2024-05-29 18:11:16 -0500

    added user repository

---
+++ backend/src/main/java/com/example/backend/models/data/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.models.data;
+
+import com.example.backend.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
